a6aeddc92c484d458c5a108968c37cb95865cb62
eelizavetochkaa <liza.bogdanova29@gmail.com>
2024-04-17 16:18:07 +0300

    рекомендации работают

--- Bars/YourRecomendations.cs
+++ Bars/YourRecomendations.cs
@@ -1,4 +1,5 @@
-﻿using Project2;
+﻿using Bars;
+using Project2;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,6 +20,85 @@ namespace ProjectTwo
             InitializeComponent();
         }
 
+        private void LoadDataFromDatabase()
+        {
+            using (var context = new CafesContext())
+            {
+                try
+                {
+                    var doskiBar = context.Bars.FirstOrDefault(b => b.Name == "DOSKI" && b.Mark == 1);
+                    var sky8 = context.Bars.FirstOrDefault(b => b.Name == "Sky8" && b.Mark == 1);
+                    var moremore = context.Bars.FirstOrDefault(b => b.Name == "More&More" && b.Mark == 1);
+
+                    var cheseria = context.Bars.FirstOrDefault(b => b.Name == "Cheseria" && b.Mark == 1);
+                    var saltbar = context.Bars.FirstOrDefault(b => b.Name == "Соль" && b.Mark == 1);
+                    var friendsbar = context.Bars.FirstOrDefault(b => b.Name == "Friends" && b.Mark == 1);
+
+                    List<Bar> barsToDisplay = new List<Bar>();
+
+                    if (doskiBar != null)
+                    {
+                        var iq = context.Bars.FirstOrDefault(b => b.Name == "IQ");
+                        if (iq != null)
+                        {
+                            barsToDisplay.Add(iq);
+                        }
+                    }
+
+                    if (sky8 != null)
+                    {
+                        var roofBar = context.Bars.FirstOrDefault(b => b.Name == "На крыше");
+                        if (roofBar != null)
+                        {
+                            barsToDisplay.Add(roofBar);
+                        }
+                    }
+                    if (moremore != null)
+                    {
+                        var marus = context.Bars.FirstOrDefault(b => b.Name == "Марусовка");
+                        if (marus != null)
+                        {
+                            barsToDisplay.Add(marus);
+                        }
+                    }
+
+                    if (cheseria != null)
+                    {
+                        var syr = context.Bars.FirstOrDefault(b => b.Name == "Сыроварня ");
+                        if (syr != null)
+                        {
+                            barsToDisplay.Add(syr);
+                        }
+                    }
+
+                    if (saltbar != null)
+                    {
+                        var shtof = context.Bars.FirstOrDefault(b => b.Name == "Штофъ");
+                        if (shtof != null)
+                        {
+                            barsToDisplay.Add(shtof);
+                        }
+                    }
+                    if (friendsbar != null)
+                    {
+                        var green = context.Bars.FirstOrDefault(b => b.Name == "Green Bottles");
+                        if (green != null)
+                        {
+                            barsToDisplay.Add(green);
+                        }
+                    }
+                    if (barsToDisplay.Count > 0)
+                    {
+                        list2.DataSource = barsToDisplay;
+                        list2.Refresh();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при подключении к базе данных: " + ex.Message);
+                }
+            }
+        }
         private void add_Click(object sender, EventArgs e)
         {
             ChoosingCollection choosing = new ChoosingCollection();
@@ -35,6 +115,7 @@ namespace ProjectTwo
             dislike.FlatAppearance.BorderSize = 0;
             back.FlatStyle = FlatStyle.Flat;
             back.FlatAppearance.BorderSize = 0;
+            LoadDataFromDatabase();
         }
 
         private void back_Click(object sender, EventArgs e)
